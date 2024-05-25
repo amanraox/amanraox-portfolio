6f0f44e8bfc67efee635670ddf639b8e65b90701
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-26 00:54:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+       sm:'640px',
+       md:'768px',
+       lg:'960px',
+       xl:'1200px', 
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
+    extend: {
+
+      colors:{
+        primary: '#121217',
+        accent: {
+          DEFAULT: '#00ff99',
+          hover: '#00e187',
+        }
+      },
+      
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
