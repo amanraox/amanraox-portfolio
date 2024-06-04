9b875ca789c7b62172ce78a455d5eff850ca5148
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-05 02:40:20 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,12 @@ const Home = () => {
           <h1 className="h1 mb-6">
             Hello I'm<br/><span className="text-accent">Aman Umrao</span>
           </h1>
-          <p className="max-w-[500px] mb-9 text-white/80  ">Sometimes I Code</br>Occasionally I Debug</br>Every so often I Tinker</br>Always I Learn</br>Consistently I Innovate</br>Here and now I have Fun</p>
+          <p className="max-w-[500px] mb-9 text-white/80  ">Sometimes I code
+          Occasionally I debug
+          Every so often I tinker
+          Always I Learn
+          Consistently I innovate
+          Here and now I have fun</p> 
           {/* btn and socials */}
           <div className="flex flex-col xl:flex-row items-center gap-8">
             <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
