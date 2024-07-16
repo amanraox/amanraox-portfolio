22c4f0f6a794cb14e834900242a258c885f8bf4d
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-17 02:55:52 +0530

    Update page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -26,21 +26,27 @@ import { motion } from "framer-motion";
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-	    <div className="px-7">
+	  <div className="px-7">
       <div className="flex flex-col md:flex-row md:space-x-4">
       <div className="flex-1 mb-4 md:mb-0">
+      <div className="relative" style={{ paddingBottom: '70%' }}>
+      <a href="https://leetcode.com/u/raoxaman/" target="_blank" rel="noopener noreferrer">
         <img 
-          src="https://leetcard.jacoblin.cool/raoxaman?font=Ubuntu&radius=13&ext=contest&sheets=https://gist.githubusercontent.com/amanraox/cf6cdd10f40353086bcb2de6694a4ce2/raw/786c10c0174e05e10c866f42ba0d47acf391b42a/leetcode_stats_card.css" 
+          src="https://leetcard.jacoblin.cool/raoxaman?border=1.5&font=Ubuntu&radius=13&ext=contest&sheets=https://gist.githubusercontent.com/amanraox/cf6cdd10f40353086bcb2de6694a4ce2/raw/786c10c0174e05e10c866f42ba0d47acf391b42a/leetcode_stats_card.css" 
           alt="SVG 2" 
-          className="w-full h-auto md:h-auto object-contain"
-        />
+          className="absolute inset-0 w-full h-full object-contain"
+        /></a>
+        </div>
       </div>
       <div className="flex-1">
+      <div className="relative" style={{ paddingBottom: '70%' }}>
+      <a href="https://codeforces.com/profile/amanloves69/" target="_blank" rel="noopener noreferrer">
         <img 
           src="https://codeforces-readme-stats.vercel.app/api/card?username=amanloves69&title_color=00ff99&text_color=00ff99&border_color=00ff99&bg_color=121217&icon_color=00ff99" 
           alt="SVG 1" 
-          className="w-full h-auto md:h-auto object-contain"
-        />
+          className="absolute inset-0 w-full h-full object-contain"
+        /></a>
+        </div>
       </div>
     </div>
     <br />
