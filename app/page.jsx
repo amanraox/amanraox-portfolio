560dd65a8f6763f9759e757b87a8d81ad3dacb77
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-20 21:43:59 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -23,10 +23,11 @@ const Home = () => {
           Here and Now I have Fun</p> 
           {/* btn and socials */}
           <div className="flex flex-col xl:flex-row items-center gap-8">
+            <a href="/cv.pdf" download className="flex items-center gap-2">
             <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
               <span>Download CV</span>
               <FiDownload className="text-xl"/>
-            </Button>
+            </Button></a>
             <div className="mb-8 xl:mb-0"><Social containerStyle="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/></div>
           </div>
         </div>
