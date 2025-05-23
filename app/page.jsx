413489eab389e8a81be399d316fd9385021755bb
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2025-05-23 06:21:51 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         {/* text */}
         <div className="text-center xl:text-left order-2 xl:order-none">
-          <span className="text-xl">Software Developer</span>
+          <span className="text-xl">Software Engineer</span>
           <h1 className="h1 mb-6">
             Hello I'm<br/><span className="text-accent">Aman Umrao</span>
           </h1>
