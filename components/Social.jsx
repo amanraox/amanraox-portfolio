36cf95fe3eebf638e3e28b9dd28ecb3485c6d7e8
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2025-12-27 05:47:16 +0530

    leetcode link added

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn, FaYoutube, FaQuora, FaWhatsapp } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
-const socials=[
-  {icon: <FaGithub />,path: "https://github.com/amanraox"},
-  {icon: <FaLinkedinIn />,path: "https://linkedin.com/in/amanraox"},
-  {icon: <FaQuora />,path: "https://www.quora.com/profile/Aman-Umrao-4"},
-  {icon: <FaWhatsapp />,path: "https://wa.me/919755857161?text=Hello%20Aman"},
-]
-const Social = ({containerStyle,iconStyles}) => {
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/amanraox" },
+  { icon: <FaLinkedinIn />, path: "https://linkedin.com/in/amanraox" },
+  { icon: <SiLeetcode />, path: "https://leetcode.com/amanraox" },
+  { icon: <FaWhatsapp />, path: "https://wa.me/919755857161?text=Hello%20Aman" },
+];
+
+const Social = ({ containerStyle, iconStyles }) => {
   return (
     <div className={containerStyle}>
-      {socials.map((item,index)=>{
+      {socials.map((item, index) => {
         return (
           <Link key={index} href={item.path} className={iconStyles}>
-          {item.icon}
+            {item.icon}
           </Link>
         );
       })}
@@ -21,4 +23,4 @@ const Social = ({containerStyle,iconStyles}) => {
   );
 };
 
-export default Social
+export default Social;
